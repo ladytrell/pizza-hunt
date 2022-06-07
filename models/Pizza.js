4d7d6076550a8491b91fdd7a6158db53d650c474
Lady Trell <antrellkent@gmail.com>
2022-06-07 18:49:00 -0400

    comment model

--- models/Pizza.js
+++ models/Pizza.js
@@ -15,9 +15,26 @@ const PizzaSchema = new Schema({
       type: String,
       default: 'Large'
     },
-    toppings: []
-  });
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]    
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false
+  }
+);
 
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
+  
   // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
